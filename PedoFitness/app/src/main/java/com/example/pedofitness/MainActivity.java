527877fb3677b2f80b2d5aef3cee35d33ea1742c
Prerit <preritshrivastava001@gmail.com>
2023-01-03 23:00:36 +0530

    app

---
+++ PedoFitness/app/src/main/java/com/example/pedofitness/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.pedofitness;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private long pressedTime;
+    public HomeFragment hf = new HomeFragment();
+
+    // creating menu-------------
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.home1:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.restart:
+                hf.restart();
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater Inflater = getMenuInflater();
+        Inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //creating fragment-----
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        replaceFragment(hf);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.main_fragment);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            }
+            if (id == R.id.report) {
+                replaceFragment(new ReportFragment());
+            }
+            if(id==R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment (Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment,fragment).commit();
+    }
+
+    //creating double click to exit-----
+    public void onBackPressed() {
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+}
+
+
